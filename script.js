ad5b2fb2e8f5acecf0cf2052eb4a664f68719522
pouyan nazarzade <poyannazarzade@gmail.com>
2025-01-26 21:13:01 -0500

    them icon changes when user clicks on it

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
     add translation 
     add easter eggs and whene ever someone finds them give them point
  */
-
+let theme;
 const toggleButton = document.getElementById('theme-toggle');
 const currentTheme = localStorage.getItem('theme');
+const IconMoon = document.getElementById('ICONmoon');
+const IconSun = document.getElementById('ICONsun');
+
+IconMoon.style.display = 'inline';
+IconSun.style.display = 'none';
+
 if(currentTheme === 'dark'){
     document.body.classList.add('dark-theme');
 }
@@ -13,11 +19,15 @@ toggleButton.addEventListener('click', () => {
     let theme = 'light';
     if(document.body.classList.contains('dark-theme')){
         theme = 'dark';
+        IconMoon.style.display = 'none';
+        IconSun.style.display = 'inline';
     }
-    
+    else{
+        theme = 'light';
+        IconMoon.style.display = 'inline';
+        IconSun.style.display = 'none';
+    }
+    // replace the class content of the sun and moon icons
     localStorage.setItem('theme', theme);
 });
 
-if(theme === 'dark'){
-     toggleButton.style.color =rgb(255, 255, 255);
-}
