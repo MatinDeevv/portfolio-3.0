65e35863f3fc91bac54b9ab49f842d768c35f845
pouyan nazarzade <poyannazarzade@gmail.com>
2025-01-29 15:27:09 -0500

    adjusted vibration

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ toggleButton.addEventListener('click', () => {
         IconMoon.style.display = 'none';
         IconSun.style.display = 'inline';
         ThemeModeSound.play();
-        navigator.vibrate(2000); 
+        navigator.vibrate(1000); 
     }
     else{
         theme = 'light';
